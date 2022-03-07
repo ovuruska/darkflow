ae6451698880d59834fb90496bb72928deae324e
Oguz Vuruskaner <ovuruska@outlook.com>
2022-03-07 14:41:09 +0300

    Added dependency installation. Added dependency installation.

--- setup.py
+++ setup.py
@@ -5,6 +5,15 @@ import numpy
 import os
 import imp
 
+
+dirname = os.path.dirname(os.path.realpath(__file__))
+requirements_path = os.path.join(dirname,"requirements.txt")
+install_requires = [] 
+if os.path.isfile(requirements_path):
+    with open(requirements_path) as f:
+        install_requires = f.read().splitlines()
+
+
 VERSION = imp.load_source('version', os.path.join('.', 'darkflow', 'version.py'))
 VERSION = VERSION.__version__
 
@@ -12,17 +21,14 @@ if os.name =='nt' :
     ext_modules=[
         Extension("darkflow.cython_utils.nms",
             sources=["darkflow/cython_utils/nms.pyx"],
-            #libraries=["m"] # Unix-like specific
             include_dirs=[numpy.get_include()]
         ),        
         Extension("darkflow.cython_utils.cy_yolo2_findboxes",
             sources=["darkflow/cython_utils/cy_yolo2_findboxes.pyx"],
-            #libraries=["m"] # Unix-like specific
             include_dirs=[numpy.get_include()]
         ),
         Extension("darkflow.cython_utils.cy_yolo_findboxes",
             sources=["darkflow/cython_utils/cy_yolo_findboxes.pyx"],
-            #libraries=["m"] # Unix-like specific
             include_dirs=[numpy.get_include()]
         )
     ]
@@ -70,5 +76,6 @@ setup(
     url='https://github.com/thtrieu/darkflow',
     packages = find_packages(),
 	scripts = ['flow'],
-    ext_modules = cythonize(ext_modules)
+    ext_modules = cythonize(ext_modules),
+    install_requires=install_requires
 )
